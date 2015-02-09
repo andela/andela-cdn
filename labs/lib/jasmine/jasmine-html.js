1ecadfb005de990bad47ce4e326fbb6e24e1f8ca
Godson Ukpere <andela-gukpere@users.noreply.github.com>
2015-02-09 09:06:42 +0100

    Update jasmine-html.js
    Fixed labs completion logic

--- labs/lib/jasmine/jasmine-html.js
+++ labs/lib/jasmine/jasmine-html.js
@@ -258,7 +258,7 @@ jasmineRequire.HtmlReporter = function(j$) {
       }
 
       //Report to AndeLabs
-      if(specsExecuted > 0 && specsExecuted - failureCount === specsExecuted) {
+      if(specsExecuted - failureCount === totalSpecsDefined) {
         console.log('Lab completed');
         Reporter.reportComplete();
       }
